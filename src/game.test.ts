da9f2ae4664d2bf1947052c0a195513d142c2e72
Michael Farrelly <mikefarrelly@gmail.com>
2024-02-21 20:17:08 -0700

    Cleaning up code

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,51 +1,44 @@
-// import { describe, expect, it } from "@jest/globals";
-
-// describe("game", () => {
-//   it("isSpare", () => {
-//     expect(isSpare({ shots: [10], score: 10, bonus: 0 })).toBe(false);
-//   });
-// });
 import { describe, expect, test } from "vitest";
-import { Game, getFrameScore, isSpare, isStrike, lastShots } from "./game";
+import { Game, getFrameScore, isSpare, isStrike } from "./game";
 
 describe("utility methods", () => {
   describe("isSpare", () => {
     test("two shots totaling 10 is a spare", () => {
-      expect(isSpare({ shots: [10, 0], score: 10, bonus: 0 })).toBe(true);
-      expect(isSpare({ shots: [2, 8], score: 10, bonus: 0 })).toBe(true);
+      expect(isSpare({ rolls: [10, 0], score: 10, bonus: 0 })).toBe(true);
+      expect(isSpare({ rolls: [2, 8], score: 10, bonus: 0 })).toBe(true);
     });
     test("strike is not a spare", () => {
-      expect(isSpare({ shots: [10], score: 10, bonus: 0 })).toBe(false);
+      expect(isSpare({ rolls: [10], score: 10, bonus: 0 })).toBe(false);
     });
     test("single shot is not a spare", () => {
-      expect(isSpare({ shots: [5], score: 5, bonus: 0 })).toBe(false);
+      expect(isSpare({ rolls: [5], score: 5, bonus: 0 })).toBe(false);
     });
   });
 
   describe("isStrike", () => {
     test("two shots totaling 10 is a not a strike", () => {
-      expect(isStrike({ shots: [10, 0], score: 10, bonus: 0 })).toBe(false);
-      expect(isStrike({ shots: [2, 8], score: 10, bonus: 0 })).toBe(false);
+      expect(isStrike({ rolls: [1, 9], score: 10, bonus: 0 })).toBe(false);
+      expect(isStrike({ rolls: [2, 8], score: 10, bonus: 0 })).toBe(false);
     });
     test("strike is successful", () => {
-      expect(isStrike({ shots: [10], score: 10, bonus: 0 })).toBe(true);
+      expect(isStrike({ rolls: [10], score: 10, bonus: 0 })).toBe(true);
     });
     test("single shot is not a strike", () => {
-      expect(isStrike({ shots: [5], score: 5, bonus: 0 })).toBe(false);
+      expect(isStrike({ rolls: [5], score: 5, bonus: 0 })).toBe(false);
     });
   });
 
   describe("getFrameScore", () => {
     test("sums the shots values", () => {
-      expect(getFrameScore({ shots: [10, 0], score: 10, bonus: 0 })).toBe(10);
-      expect(getFrameScore({ shots: [2, 8], score: 10, bonus: 0 })).toBe(10);
-      expect(getFrameScore({ shots: [2], score: 2, bonus: 0 })).toBe(2);
-      expect(getFrameScore({ shots: [10, 3, 4], score: 17, bonus: 0 })).toBe(
+      expect(getFrameScore({ rolls: [10, 0], score: 10, bonus: 0 })).toBe(10);
+      expect(getFrameScore({ rolls: [2, 8], score: 10, bonus: 0 })).toBe(10);
+      expect(getFrameScore({ rolls: [2], score: 2, bonus: 0 })).toBe(2);
+      expect(getFrameScore({ rolls: [10, 3, 4], score: 17, bonus: 0 })).toBe(
         17
       );
-      expect(getFrameScore({ shots: [0, 0], score: 0, bonus: 0 })).toBe(0);
+      expect(getFrameScore({ rolls: [0, 0], score: 0, bonus: 0 })).toBe(0);
 
-      expect(getFrameScore({ shots: [0], score: 0, bonus: 0 })).toBe(0);
+      expect(getFrameScore({ rolls: [0], score: 0, bonus: 0 })).toBe(0);
     });
   });
 
@@ -66,68 +59,145 @@ describe("utility methods", () => {
           bonus: 20,
           frame: 1,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 2,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 3,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 4,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 5,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 6,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 7,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 8,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 9,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 20,
           frame: 10,
           score: 10,
-          shots: [10, 10, 10],
+          rolls: [10, 10, 10],
         },
       ]);
       expect(game.finalScore()).toBe(300);
     });
-  });
 
-  describe("game", () => {
+    test("almost perfect game", () => {
+      let game = new Game();
+
+      // almost perfect, spare in frame 10, and a bonus 5
+      const rolls = [
+        10, 10, 10, 10, 10, 10, 10, 10, 10, /* spare: */ 2, 8,
+        /* extra throw: */ 5,
+      ];
+      for (const roll of rolls) {
+        game.roll(roll);
+      }
+
+      expect(
+        game.scoreByFrame.map((f, i) => ({ frame: i + 1, ...f }))
+      ).toStrictEqual([
+        {
+          bonus: 20,
+          frame: 1,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 20,
+          frame: 2,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 20,
+          frame: 3,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 20,
+          frame: 4,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 20,
+          frame: 5,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 20,
+          frame: 6,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 20,
+          frame: 7,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 12,
+          frame: 8,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 10,
+          frame: 9,
+          score: 10,
+          rolls: [10],
+        },
+        {
+          bonus: 5,
+          frame: 10,
+          score: 10,
+          rolls: [2, 8, 5],
+        },
+      ]);
+      expect(game.finalScore()).toBe(267);
+    });
+
     test("short game sums the shots values", () => {
       let game = new Game();
 
@@ -141,29 +211,27 @@ describe("utility methods", () => {
         {
           bonus: 20,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 10,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 0,
           score: 10,
-          shots: [10],
+          rolls: [10],
         },
         {
           bonus: 0,
           score: 0,
-          shots: [],
+          rolls: [],
         },
       ]);
       expect(game.finalScore()).toBe(60);
     });
-  });
 
-  describe("game", () => {
     test("normal short game", () => {
       let game = new Game();
 
@@ -176,28 +244,26 @@ describe("utility methods", () => {
         {
           bonus: 0,
           score: 5,
-          shots: [2, 3],
+          rolls: [2, 3],
         },
         {
           bonus: 0,
           score: 9,
-          shots: [4, 5],
+          rolls: [4, 5],
         },
         {
           bonus: 0,
           score: 0,
-          shots: [],
+          rolls: [],
         },
       ]);
       expect(game.finalScore()).toStrictEqual(14);
     });
-  });
 
-  describe("game", () => {
     test("scoring a spare", () => {
       let game = new Game();
 
-      // perfect game
+      // first frame spare, following by a non-spare, non-strike
       const rolls = [2, 8, 4, 5];
       for (const roll of rolls) {
         game.roll(roll);
@@ -206,65 +272,30 @@ describe("utility methods", () => {
         {
           bonus: 4,
           score: 10,
-          shots: [2, 8],
+          rolls: [2, 8],
         },
         {
           bonus: 0,
           score: 9,
-          shots: [4, 5],
+          rolls: [4, 5],
         },
         {
           bonus: 0,
           score: 0,
-          shots: [],
+          rolls: [],
         },
       ]);
       expect(game.finalScore()).toStrictEqual(23);
     });
-  });
 
-  describe("lastShots", () => {
-    test("shows all the shots", () => {
-      expect(
-        lastShots(
-          [
-            {
-              bonus: 0,
-              score: 5,
-              shots: [2, 3],
-            },
-            {
-              bonus: 0,
-              score: 9,
-              shots: [4, 5],
-            },
-          ],
-          3
-        )
-      ).toStrictEqual([3, 4, 5]);
-
-      expect(
-        lastShots(
-          [
-            {
-              bonus: 0,
-              score: 10,
-              shots: [10],
-            },
-            {
-              bonus: 0,
-              score: 10,
-              shots: [10],
-            },
-            {
-              bonus: 0,
-              score: 10,
-              shots: [10],
-            },
-          ],
-          3
-        )
-      ).toStrictEqual([10, 10, 10]);
+    test("throws error when rolling more than 12 rolls", () => {
+      expect(() => {
+        let game = new Game();
+        const rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+        for (const roll of rolls) {
+          game.roll(roll);
+        }
+      }).toThrowError(/No more rolls allowed/);
     });
   });
 });
--- src/game.ts
+++ src/game.ts
@@ -1,54 +1,9 @@
-interface Frame {
-  shots: number[];
-  score: number;
-  bonus: number;
-  //   isStrike: boolean;
-  //   isSpare: boolean;
-}
-
-/**
- * Get the shot values
- */
-export function getFrameScore(frame: Frame): number {
-  // sum up the frame's shots
-  return frame.shots.reduce((prev, curr) => {
-    return prev + curr;
-  }, 0);
-}
-
-/**
- * Test if the frame is a Strike
- * - only 1 shot
- * - first shot is 10 pins.
- */
-export function isStrike(frame: Frame): boolean {
-  return frame.shots.length == 1 && frame.shots[0] === 10;
-}
-
-/**
- * Test if the frame is a Spare
- * - only 2 shot
- * - sum of shots is 10 pins.
- */
-export function isSpare(frame: Frame): boolean {
-  return frame.shots.length > 1 && getFrameScore(frame) === 10;
-}
-
-export function lastShots(frames: Frame[], num: number = 3): number[] {
-  const maxIndex = frames.length - 1;
-  const x = frames.reduceRight((prev, curr) => {
-    return [...curr.shots, ...prev];
-  }, [] as number[]);
-
-  return x.slice(-num);
-}
-
 export class Game {
   frameNumber: number = 1; // starting at 1.
   // shot: number = 0;
   scoreByFrame: Frame[] = [
     {
-      shots: [],
+      rolls: [],
       score: 0,
       bonus: 0,
     },
@@ -60,138 +15,95 @@ export class Game {
   roll(pins: number): void {
     const currentFrameIndex = this.frameNumber - 1;
     const currentFrame = this.scoreByFrame[currentFrameIndex];
-    const currentShot = this.scoreByFrame[currentFrameIndex].shots.length + 1; // 1-base numbers
-    currentFrame.shots.push(pins);
-    console.log(
-      `${this.frameNumber}/${currentShot}: [${currentFrame.shots.join(",")}]`
-    );
+    const prevFrame = this.scoreByFrame[currentFrameIndex - 1];
+    const twoPrevFrame = this.scoreByFrame[currentFrameIndex - 2];
+    const currentRoll = currentFrame.rolls.length + 1;
+
+    if (this.frameNumber == 10 && currentFrame.rolls.length == 3) {
+      throw Error("No more rolls allowed!");
+    }
+
+    currentFrame.rolls.push(pins);
 
     // when previous frame is a strike, add pins to its bonus
-    if (
-      this.frameNumber > 1 &&
-      (this.frameNumber < 10 || (this.frameNumber == 10 && currentShot == 1)) &&
-      isStrike(this.scoreByFrame[currentFrameIndex - 1])
-    ) {
-      // previous frame was a strike
-      // console.log(`${this.frame}: prev was strike`);
-      console.log(
-        `${this.frameNumber}/${currentShot}/bonus to ${
-          currentFrameIndex - 1
-        }: ${pins}`
-      );
-      this.scoreByFrame[currentFrameIndex - 1].bonus += pins;
+    if (this.frameNumber > 1 && this.frameNumber < 10 && isStrike(prevFrame)) {
+      prevFrame.bonus += pins;
     }
 
     // when 2-previous frame was a strike, add pins to bonus.
     if (
       this.frameNumber > 2 &&
       this.frameNumber < 10 &&
-      isStrike(this.scoreByFrame[currentFrameIndex - 2]) &&
-      isStrike(this.scoreByFrame[currentFrameIndex - 1])
+      isStrike(twoPrevFrame) &&
+      isStrike(prevFrame)
     ) {
-      // console.log(`${this.frame}: prev 2x was strike`);
-      console.log(
-        `${this.frameNumber}/${currentShot}/bonus to ${
-          currentFrameIndex - 2
-        }: ${pins}`
-      );
-      this.scoreByFrame[currentFrameIndex - 2].bonus += pins;
+      twoPrevFrame.bonus += pins;
     }
-    if (
-      this.frameNumber == 10 &&
-      currentShot == 1 &&
-      isStrike(this.scoreByFrame[currentFrameIndex - 2]) &&
-      isStrike(this.scoreByFrame[currentFrameIndex - 1])
-    ) {
-      console.log(
-        `${this.frameNumber}/${currentShot}/10 bonus to ${
-          currentFrameIndex - 2
-        }: ${pins}`
-      );
-      this.scoreByFrame[currentFrameIndex - 2].bonus += pins;
-    }
-    if (
-      this.frameNumber == 10 &&
-      currentShot == 2 &&
-      isStrike(this.scoreByFrame[currentFrameIndex - 1])
-    ) {
-      console.log(
-        `${this.frameNumber}/${currentShot}/10 bonus to ${currentFrameIndex}: ${pins}`
-      );
-      this.scoreByFrame[currentFrameIndex].bonus += pins;
-    }
-    if (
-      this.frameNumber == 10 &&
-      currentShot == 3 &&
-      this.scoreByFrame[currentFrameIndex].shots[0] == 10
-    ) {
-      console.log(
-        `${this.frameNumber}/${currentShot}/10 bonus to ${currentFrameIndex}: ${pins}`
-      );
-      this.scoreByFrame[currentFrameIndex].bonus += pins;
+    // conditions of the last frame
+    if (this.frameNumber == 10) {
+      if (currentRoll == 1 && isStrike(twoPrevFrame) && isStrike(prevFrame)) {
+        twoPrevFrame.bonus += pins;
+      }
+      if (currentRoll < 3 && isStrike(prevFrame)) {
+        prevFrame.bonus += pins;
+      }
+      if (currentRoll > 1 && currentRoll < 3 && isStrike(currentFrame)) {
+        currentFrame.bonus += pins;
+      }
+      // from a strike or spare, just add the pin count to the bonus
+      if (currentRoll == 3) {
+        currentFrame.bonus += pins;
+      }
     }
 
     if (
       this.frameNumber > 1 &&
-      currentShot == 1 &&
-      isSpare(this.scoreByFrame[currentFrameIndex - 1])
+      this.frameNumber < 10 &&
+      currentRoll == 1 &&
+      isSpare(prevFrame)
     ) {
-      // previous frame was a spare
-      console.log(
-        `${this.frameNumber}/${currentShot}/bonus to ${
-          currentFrameIndex - 1
-        }: ${pins}`
-      );
-      this.scoreByFrame[currentFrameIndex - 1].bonus += pins;
+      prevFrame.bonus += pins;
     }
 
-    // user rolls a strike
-    if (
-      currentShot == 1 &&
-      this.frameNumber < 10 &&
-      isStrike(this.scoreByFrame[currentFrameIndex])
-    ) {
-      this.advanceFrame();
-      return;
-    }
-
-    // user rolls a spare
-    if (
-      currentShot == 2 &&
-      this.frameNumber < 10 &&
-      isSpare(this.scoreByFrame[currentFrameIndex])
-    ) {
-      this.advanceFrame();
-      return;
-    }
-    if (
+    if (this.frameNumber < 10) {
+      // user rolls a strike
+      if (isStrike(currentFrame)) {
+        this.advanceFrame();
+        return;
+      } else if (isSpare(currentFrame)) {
+        // user rolls a spare
+        this.advanceFrame();
+        return;
+      } else if (currentRoll == 2) {
+        // normally only allow 2 shots except on frame 10.
+        this.advanceFrame();
+      }
+    } else if (
       this.frameNumber == 10 &&
-      getFrameScore(this.scoreByFrame[currentFrameIndex]) === 10
+      // currentFrame.shots.length <= 2 &&
+      // strike or spare
+      (isStrike(currentFrame) || isSpare(currentFrame)) &&
+      getFrameScore(currentFrame) === 10
     ) {
-      this.scoreByFrame[this.frameNumber - 1].score = getFrameScore(
-        this.scoreByFrame[this.frameNumber - 1]
-      );
-      console.log(`allow more shots`);
-      // allow more shots
-      return;
-    }
-
-    // normally only allow 2 shots except on frame 10.
-    if (this.frameNumber != 10 && currentShot == 2) {
-      this.advanceFrame();
+      currentFrame.score = getFrameScore(currentFrame);
+      // don't advance to the next frame
     }
   }
 
-  advanceFrame(): void {
-    console.log(`advance frame`);
-    // write the score
-    this.scoreByFrame[this.frameNumber - 1].score = getFrameScore(
-      this.scoreByFrame[this.frameNumber - 1]
-    );
+  getCurrentFrame(): Frame {
+    return this.scoreByFrame[this.frameNumber - 1];
+  }
 
+  advanceFrame(): void {
+    // write the score for current frame
+    this.getCurrentFrame().score = getFrameScore(this.getCurrentFrame());
+
+    // advanced frame number
     this.frameNumber++;
+
+    // insert the next frame
     this.scoreByFrame.push({
-      shots: [],
+      rolls: [],
       score: 0,
       bonus: 0,
     });
@@ -205,4 +117,43 @@ export class Game {
       return prevFrameScore + currentFrame.score + currentFrame.bonus;
     }, 0);
   }
+}
+
+interface Frame {
+  rolls: number[];
+  score: number;
+  bonus: number;
+}
+
+/**
+ * Get the shot values
+ */
+export function getFrameScore(frame: Frame): number {
+  // sum up the frame's shots
+  return frame.rolls.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+}
+
+/**
+ * Test if the frame is a Strike
+ * - only 1 shot
+ * - first shot is 10 pins.
+ */
+export function isStrike(frame: Frame): boolean {
+  return frame.rolls.length >= 1 && frame.rolls[0] === 10;
+}
+
+/**
+ * Test if the frame is a Spare
+ * - only 2 shot
+ * - sum of shots is 10 pins.
+ */
+export function isSpare(frame: Frame): boolean {
+  return (
+    frame.rolls.length > 1 &&
+    frame.rolls.slice(0, 2).reduce((prev, curr) => {
+      return prev + curr;
+    }, 0) === 10
+  );
 }
